d49f9287a516560bf703e68764d15df9e10dda67
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-20 16:56:37 +0000

    Overhaul UI with a futuristic theme and grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FetchData from "./Components/FetchData";
 import FetchYogaData from "./Components/FetchYogaData";
+import "./Components/FetchData.css";
 
 function App() {
   return (
