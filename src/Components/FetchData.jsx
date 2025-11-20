91618d43c94bdbcef00c3368d17eedf644e65e52
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-20 16:28:56 +0000

    Integrated useFetch hook into FetchData component

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import useFetch from "./UseFetch";
 
 const FetchData = () => {
+  const [data] = useFetch("https://api.npoint.io/9045c260b1565daa9e15");
+  console.log(data);
+
   return (
     <>
-     <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-     </ul>
+      <ul className="list_data_main">
+        <h1 className="usefetch_heading">Use Fetch Custom Hook</h1>
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default FetchData
+export default FetchData;
